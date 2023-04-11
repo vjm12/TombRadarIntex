8b8b94adad827af93ebe0d5ef2286a0fc52301bc
vjm12 <vjm12@byu.edu>
2023-04-11 10:54:03 -0600

    Add project files.

---
+++ CustomUserManagement.MVC-master/UserManagement.MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Intex2023.Data;
+
+[assembly: HostingStartup(typeof(Intex2023.Areas.Identity.IdentityHostingStartup))]
+namespace Intex2023.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
